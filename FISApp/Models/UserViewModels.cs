6807eed5bd718acd5ff02c87b332e9b2b634bea6
Rai <hieppdse03186@fpt.edu.vn>
2016-12-10 20:20:17 +0700

    a

--- FISApp/Models/UserViewModels.cs
+++ FISApp/Models/UserViewModels.cs
@@ -16,9 +16,6 @@ namespace FISApp.Models
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
     }
 
     public class Profile
